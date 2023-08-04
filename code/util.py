752911caf2f72fb2c1ef787c0c6905434f8c308c
John Trimble <trimblej@gmail.com>
2023-08-04 09:45:32 +0000

    initial commit

---
+++ code/util.py
@@ -0,0 +1,186 @@
+import csv
+import json
+import time
+from pathlib import Path
+import torch
+import numpy as np
+import torchvision.transforms as transforms
+
+from torchvision.datasets import ImageFolder
+from torchvision.datasets.folder import pil_loader
+from torch.utils.data import Dataset
+
+from PIL import Image
+
+import matplotlib.pyplot as plt
+
+
+class BestArtDataset(ImageFolder):
+    def __init__(self, root, transform=None):
+        super(BestArtDataset, self).__init__(root, transform=transform)
+        
+    def __getitem__(self, index):
+        path, _ = self.samples[index]
+
+        # Get the name of the file
+        filename = path
+
+        # Call super method
+        sample, target = super(BestArtDataset, self).__getitem__(index)
+
+        return sample, filename, target
+
+
+class WikiArtDataset(Dataset):
+    def __init__(self, root, transform=None):
+        super(WikiArtDataset, self).__init__()
+        self.transform = transform
+        self.root = Path(root)
+
+        # List directories in root
+        self.style_classes = [str(d.name) for d in self.root.iterdir() if d.is_dir()]
+        self.style_class_to_idx = {class_name: i for i, class_name in enumerate(self.style_classes)}
+
+        # https://huggingface.co/datasets/huggan/wikiart/tree/main
+        huggingface_metadata = self.root / "dataset_infos.json"
+        with open(huggingface_metadata) as f:
+            data_infos = json.loads(f.read())
+            for feature in ['artist']:
+                classes = data_infos['huggan--wikiart']['features'][feature]['names']
+                setattr(self, f'{feature}_classes', tuple(classes))
+                setattr(self, f'{feature}_class_to_idx', {class_name: i for i, class_name in enumerate(classes)})
+        
+        classes_csv = self.root / "wclasses.csv"
+        self.samples = []
+
+        with open(classes_csv) as f:
+            reader = csv.reader(f)
+            # For every row in the csv file
+            for i, (filename, artist_idx, genre_idx, style_idx) in enumerate(reader):
+                # skip header
+                if i == 0:
+                    continue
+                self.samples.append((filename, int(artist_idx)))
+
+    def __getitem__(self, index):
+        filename, artist_idx = self.samples[index]
+        path = self.root / filename
+        img = pil_loader(path)
+        if self.transform is not None:
+            img = self.transform(img)
+        return img, filename, artist_idx, self.style_class_to_idx[path.parent.name]
+
+    def __len__(self):
+        return len(self.samples)
+
+
+def tensor2im(tensor):
+    """
+    Convert a PyTorch tensor using imagenet normalization to PIL Image
+    """
+    tensor = tensor.squeeze().detach().cpu()
+    tensor = tensor * torch.Tensor([0.229, 0.224, 0.225]).view(3, 1, 1)
+    tensor = tensor + torch.Tensor([0.485, 0.456, 0.406]).view(3, 1, 1)
+    tensor = tensor * 255
+    tensor = tensor.clip(0, 255)
+    tensor = tensor.permute(1, 2, 0)
+    return Image.fromarray(tensor.numpy().astype('uint8'))
+
+
+def display_img_row(*imgs, titles=None):
+    if titles is None:
+        titles = [None] * len(imgs)
+
+    fig, ax = plt.subplots(1, len(imgs), figsize=(10, 10))
+    for i, (img, title) in enumerate(zip(imgs, titles)):
+        ax[i].imshow(tensor2im(img))
+        if title is not None:
+            ax[i].set_title(title)
+        ax[i].axis('off')
+    plt.show()
+
+
+@torch.no_grad()
+def create_grid(feature_maps, output_size):
+    """
+    Creates a grid of feature maps with the given output size,
+    resizing feature maps as needed.
+    """
+    output_height, output_width  = output_size
+
+    # Remove batch dimension if present
+    if len(feature_maps.shape) == 4:
+        feature_maps = feature_maps.squeeze()
+
+    # Rescale all values to be between 0 and 1
+    feature_maps -= feature_maps.min()
+    feature_maps /= feature_maps.max()
+
+    # Get the number of feature maps
+    n = feature_maps.shape[0]
+
+    # Get the number of rows and columns
+    cols = int(np.ceil(np.sqrt(n)))
+    rows = int(np.ceil(n / cols))
+
+    # Resize the feature maps
+    gap = 2
+    map_width = (output_width - (cols - 1)*gap) // cols
+    map_height = (output_height - (cols -1)*gap) // cols
+    
+    # feature_maps is a tensor of size (N, C, H, W), we want to resize it to (N, C, map_height, map_width)
+    feature_maps = torch.nn.functional.interpolate(feature_maps.unsqueeze(0), size=(map_height, map_width), mode='bilinear')
+    feature_maps = feature_maps.squeeze(0)
+
+    # Build the grid
+    grid = torch.zeros((800, 800))
+    mask = torch.zeros((800, 800))
+    for i in range(rows):
+        for j in range(cols):
+            fm_idx = i*cols + j
+            if fm_idx >= len(feature_maps):
+                continue
+            fm = feature_maps[fm_idx]
+            start_width = i*(map_width + gap)
+            end_width =  start_width + map_width
+            start_height = j*(map_height + gap)
+            end_height = start_height + map_height
+            grid[start_width:end_width, start_height:end_height] = fm
+            mask[start_width:end_width, start_height:end_height] = 1
+    
+    # Generate greyscale PIL image with 3 channels
+    grid = grid.unsqueeze(0).repeat(3, 1, 1)
+    grid = grid.numpy().transpose(1, 2, 0)
+
+    # Apply mask as an alpha channel
+    mask = mask.unsqueeze(0)
+    mask = mask.numpy().transpose(1, 2, 0)
+    grid = np.concatenate((grid, mask), axis=2)
+
+    grid = Image.fromarray((grid*255).astype('uint8'), mode='RGBA')
+    return grid
+
+
+def generate_feature_map_images(features, output_size, prefix):
+    for name, feature_maps in features.items():
+        if not 'fc' in name:
+            grid = create_grid(feature_maps, output_size)
+            grid.save(f'{prefix}_{name}.png')
+
+
+def with_retries(fn, max_retries=5, interval=10, max_interval=60):
+    output = None
+    retries = 0
+    while True:
+        try:
+            output = fn()
+            break
+        except Exception as e:
+            if retries < max_retries:
+                interval = interval*2
+                interval = min(interval, max_interval)
+                retries += 1
+                time.sleep(interval)
+            else:
+                raise e
+    return output
